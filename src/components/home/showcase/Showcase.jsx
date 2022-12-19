1c9d068d28ac7eb5de61346b019a9207cf13d4c2
ferrarajohn <94008222+ferrarajohn@users.noreply.github.com>
2022-12-18 21:19:10 -0500

    Add files via upload

---
+++ src/components/home/showcase/Showcase.jsx
@@ -0,0 +1,258 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./showcase.css"
+
+const Showcase = () => {
+    return (
+     
+     
+        <div className='showcase'>
+
+            <div class="slider">
+	<div class="slide-track">
+		<div class="slide">
+       <a href="https://delorean.markets/market)"><img src="Images/1.png" height="150" width="240" alt="" /></a> 
+       <div class="carr-title">Degen Toonz</div>
+
+
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/2.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Bulls&Apes</p>
+
+
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/3.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Fluf Project</p>
+
+
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/4.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">MA YC Project</p>
+
+
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/5.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Mob Ties</p>
+
+
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/6.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Party Bear</p>
+
+
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/7.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Lazy Lions</p>
+
+
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/8.png" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Prime Apes</p>
+
+
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/9.png" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Cyber Brokers</p>
+
+
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/1.png" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Inhabitas</p>
+
+        
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/2.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Killa Bears</p>
+
+        
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/3.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">ToyMories</p>
+
+        
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/4.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Buzzies</p>
+
+        
+		</div>
+		<div class="slide">
+        <a href="https://delorean.markets/market)"><img src="Images/5.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">CryptoMories</p>
+
+        
+		</div>
+	</div>
+
+</div>
+
+<div class="slider2">
+	<div class="slide-track2">
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/6.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Degen Toonz</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/7.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Bulls&Apes</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/8.png" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Fluf Project</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/9.png" height="150" width="240" alt="" /></a>
+        <p class="carr-title">MA YC Project</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/1.png" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Mob Ties</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/2.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Party Bear</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/3.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Lazy Lions</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/4.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Prime Apes</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/5.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Cyber Brokers</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/6.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Inhabitas</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/7.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Killa Bears</p>
+
+        
+		</div>
+		<div class="slide2">
+        <a href="https://delorean.markets/market)"><img src="Images/8.png" height="150" width="240" alt="" /></a>
+        <p class="carr-title">ToyMories</p>
+
+        
+		</div>
+		<div class="slide2">
+        
+        <a href="https://delorean.markets/market)"><img src="Images/9.png" height="150" width="240" alt="" /></a>
+        <p class="carr-title">Buzzies</p>
+        
+		</div>
+		<div class="slide2">
+        
+        <a href="https://delorean.markets/market)"><img src="Images/1.jpg" height="150" width="240" alt="" /></a>
+        <p class="carr-title">CryptoMories</p>
+        
+		</div>
+	</div>
+
+  
+</div>
+
+            <div className='main-txt'>
+                Purchase and create royalty-linked items! 
+            </div>
+
+        <div className="second-main-container">
+            <div className="flex-container">
+            <div className="second-main-title">
+                <h1>RESOURCES</h1>
+            </div>
+            <div className="first-item">
+                <div className="item1">
+                
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, laborum.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, laborum.</p>
+                    
+                </div>
+                <div className="item2">
+                
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, voluptatibus?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, laborum.</p>
+                </div>
+                <div className="item3">
+                
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, qui.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, laborum.</p>
+                </div>
+                </div>
+            </div>
+            <div className="second-item">
+                <div className="image1">
+                    <img className="second-item-image"src="https://i.seadn.io/gae/2QsFxb14us6FSDu_Rkj4cXflv_ijntnySy_a3jDah0fS5TbszBKbye4wek0aNHWGZqkeQ1CmlqNUJf9TEIel7tcmFgTvPTK-dVAWmXM?auto=format&w=3840" alt="" />
+                </div>
+                <div className="image2">
+                <img className="second-item-image"src="https://i.seadn.io/gae/X4Q2rHfwRSUHRw9HLkKfG3J-8NjjhUTXV3sJ1CVvCtg-ycL-hHroQMiEUP9pqRNJvV7mjH2euA1jDxKvS1mhgQI-ZUljlhI-owP2GA?auto=format&w=3840" alt="" />
+                </div>
+                <div className="image3">
+                <img className="second-item-image"src="https://i.seadn.io/gae/ibjUDVjAvvT4yN5uuplWVSL7q9SD3WdDhJWkpdkHtT-NkOCdnKIuFVzdntP4sQ2BcLyf2I2hwFI-IdaxUOqXBZ6g0jS_jcR9ud2Syg?auto=format&w=3840" alt="" />
+                </div>
+                <div className="image4">
+                <img className="second-item-image"src="https://i.seadn.io/gae/YwG6cplD6vUrJzksp-BXVeYhQfroGPt0OVmXf-6t9dnblRkfgGdj_YUMXlPTqfzWnKwovYHRD3ud0zO_KMcadflBqcBsfdUm6YpOqA?auto=format&w=3840" alt="" />
+                <button class="btn"> <a href="/link"> Link a Code</a></button>
+                </div>
+            </div>
+        </div>
+
+            {/* <div className='sec-txt'>
+                Delorean Codes is the world’s first, royalty-incorporated gateway between physical goods and blockchain technology
+            </div> */}
+            
+
+            <div className="news-container">
+                <div className="show-btn-wrap">
+                <Link to="/link" className="btn showcase-btn light-btn">Get Codes</Link>
+                </div>
+                <div className="email-container">
+                    <h1 class="news-title">Sign up for updates on Delorean!</h1>
+                    <div className="news-flex">
+                    <input class="email" type="email" id="email" name="email" placeholder="Enter your e-mail."/>
+                    <input class="submit" type="submit" />
+                    </div>
+                </div>
+            </div>
+
+        
+
+        </div>
+    )
+}
+
+export default Showcase
